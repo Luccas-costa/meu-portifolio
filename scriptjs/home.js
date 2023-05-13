e994fa6cefdfa871e79e1c4c0ddba42fd1e655b4
luccas costa <lucaspcosta70@gmail.com>
2023-05-13 17:37:18 -0300

    -.-

--- scriptjs/home.js
+++ scriptjs/home.js
@@ -4,6 +4,6 @@ function paginloginBR(){
 
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav12").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
-}
+  }
