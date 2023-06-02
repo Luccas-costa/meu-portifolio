674cf79cc1870173bc510050e5baec4435b509db
luccas costa <lucaspcosta70@gmail.com>
2023-06-02 17:55:20 -0300

    hours

--- scriptjs/home.js
+++ scriptjs/home.js
@@ -7,3 +7,18 @@ function menuOnClick() {
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+
+// sitema de horas da saldaçao
+
+let saldacao = document.getElementById("saldacao");
+let hours = new Date().getHours();
+
+
+if (hours < 12) {
+    console.log("bom dia")
+}else if (hours < 18) {
+    document.getElementById("saldacao").innerHTML="Boa tarde"; 
+}else {
+    document.getElementById("saldacao").innerHTML="Boa noite"
+}
